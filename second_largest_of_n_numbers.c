8f5ee214f69de011e775f1810798f1a052015b2f
Anusmita315 <jhaanusmita@gmail.com>
2025-05-03 01:17:05 +0530

    Update second_largest_of_n_numbers.c

--- second_largest_of_n_numbers.c
+++ second_largest_of_n_numbers.c
@@ -1,25 +1,42 @@
 // Write a program to find the second largest of n numbers using an array.
-
 #include <stdio.h>
 
 int main() {
-    int n, i, minIndex;
-    printf("Enter the number of elements: ");
+    int n, i, first, second;
+    printf("Enter the number of elements (at least 2): ");
     scanf("%d", &n);
+
+    if (n < 2) {
+        printf("At least two numbers are required.\n");
+        return 1;
+    }
     int arr[n];
     printf("Enter %d numbers:\n", n);
     for(i = 0; i < n; i++) {
         printf("Element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
-    minIndex = 0;
-    for(i = 1; i < n; i++) {
-        if(arr[i] < arr[minIndex]) {
-            minIndex = i;
+    if (arr[0] > arr[1]) {
+        first = arr[0];
+        second = arr[1];
+    } else {
+        first = arr[1];
+        second = arr[0];
+    }
+    for(i = 2; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
         }
     }
 
-    printf("The smallest number is %d and its position is %d.\n", arr[minIndex], minIndex + 1);
+    if (first == second) {
+        printf("All elements are equal or there is no distinct second largest number.\n");
+    } else {
+        printf("The second largest number is: %d\n", second);
+    }
 
     return 0;
 }
