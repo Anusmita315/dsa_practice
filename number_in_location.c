6ced076ca907dbfb73bde1965ec587e3ec2586c6
Anusmita315 <jhaanusmita@gmail.com>
2025-05-03 01:13:14 +0530

    Update number_in_location.c

--- number_in_location.c
+++ number_in_location.c
@@ -4,41 +4,27 @@
 
 int main() {
     int arr[100], n, i, pos, num;
-
-    // Input the number of elements
     printf("Enter the number of elements (max 100): ");
     scanf("%d", &n);
-
-    // Input array elements
     printf("Enter %d elements:\n", n);
     for(i = 0; i < n; i++) {
         printf("Element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
-
-    // Input the number to insert and position
     printf("Enter the number to insert: ");
     scanf("%d", &num);
 
     printf("Enter the position to insert at (1 to %d): ", n + 1);
     scanf("%d", &pos);
-
-    // Validate position
     if(pos < 1 || pos > n + 1) {
         printf("Invalid position!\n");
         return 1;
     }
-
-    // Shift elements to the right
     for(i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
-
-    // Insert the number
     arr[pos - 1] = num;
     n++;
-
-    // Display the updated array
     printf("Array after insertion:\n");
     for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
